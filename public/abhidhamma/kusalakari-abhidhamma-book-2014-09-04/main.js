f09b784f85086f37f0ab9c4f3831a7be7ba0a4f7
aung <aungloo@gmail.com>
2020-04-20 01:38:47 -0700

    cleanup

--- public/abhidhamma/kusalakari-abhidhamma-book-2014-09-04/main.js
+++ public/abhidhamma/kusalakari-abhidhamma-book-2014-09-04/main.js
@@ -1,24 +1,34 @@
 debug = false;
+function w2u(text) {
+    return convertText(text, 'wininnwa', 'unicode');
+}
 
 $(document).ready(function () {
-    console.log("ready!");
+
+    function onLinkClick() {
+        var pageTitle = frames[1].document.title;
+        var pageNum = pageTitle.replace('Page ', '');
+        var pageTitleDiv = '<div>' + pageTitle +'</div>'
+        frames[1].document.body.innerHTML = pageTitleDiv + frames[1].document.body.innerHTML;
+
+        // history.pushState({page: pageNum}, pageTitle, "?page=" + pageNum)
+    }
+
     $('#links').on('load', function () {
-        function onLinkClick() {
-            console.log(arguments);
-            var pList = frames[1].document.getElementsByTagName('p') 
-            console.log(pList);
-            for (var i = 0; i < pList.length; i++) {
-                var text = pList.item(i).innerHTML.replace('<br>', '').replace('&nbsp', '')
-                var unicodeText = convertText(text, 'wininnwa', 'unicode');
-                // console.log('from', text, 'to', unicodeText)
-                // console.log(unicodeText);
+        var list = frames[0].document.getElementsByTagName('a');
+        for (var i = 0; i < list.length; i++) {
+            list.item(i).onclick = function() {
+                setTimeout(onLinkClick, 200);
             }
         }
-        console.log("links ready!");
-        var list = frames[0].document.getElementsByTagName('a');
-        console.log(list);
-        for (var i = 0; i < list.length; i++) {
-            list.item(i).onclick = onLinkClick;
-        }
     })
+
+    // todo: simulate page link click
+    setTimeout(function() {
+        var pageTitle = frames[1].document.title;
+        var pageNum = pageTitle.replace('Page ', '');
+        var pageTitleDiv = '<div>' + pageTitle +'</div>'
+        frames[1].document.body.innerHTML = pageTitleDiv + frames[1].document.body.innerHTML;
+        //console.log(frames[1].document.title)
+    }, 200)
 });
